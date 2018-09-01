37e12f35bbb6ed92042360d3742fab33d73c8637
John-Senga <2015081653@ust-ics.mygbiz.com>
2018-09-01 15:56:04 +0800

    fixed relative layout

--- app/src/main/java/com/senga/john/sengajohnlab1/MainActivity.java
+++ app/src/main/java/com/senga/john/sengajohnlab1/MainActivity.java
@@ -8,8 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.relative_layout);
+        setContentView(R.layout.relative_layout);
         //setContentView(R.layout.linear_layout);
-        setContentView(R.layout.constraint_layout);
+        //setContentView(R.layout.constraint_layout);
     }
 }
