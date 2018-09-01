63372ca22d1b3f2840d193364e07eb422e75d2c8
John-Senga <2015081653@ust-ics.mygbiz.com>
2018-09-01 12:08:22 +0800

    Initial Commit

---
+++ app/src/main/java/com/senga/john/sengajohnlab1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.senga.john.sengajohnlab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout);
+        //setContentView(R.layout.linear_layout);
+        //setContentView(R.layout.constraint_layout);
+    }
+}
